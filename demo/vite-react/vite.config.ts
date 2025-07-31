f408e79c369ef9fdd4c2c1341a0ba496bad99450
HichemTab-tech <konanhichemsinshi@gmail.com>
2025-07-31 22:16:51 +0100

    fix: update Vite configuration to ignore specific files during server watch

--- demo/vite-react/vite.config.ts
+++ demo/vite-react/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import TsRuntimePickerVitePlugin from "ts-runtime-picker/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TsRuntimePickerVitePlugin()],
+    plugins: [react(), TsRuntimePickerVitePlugin()],
+    server: {
+        watch: {
+            ignored: ['!**/node_modules/ts-runtime-picker/**'],
+        },
+    },
+
 })
